2867b4394468bb0f12b720caec5992572897ef17
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-12-03 13:14:01 -0600

    Update gather-stats.sh

--- gather-stats.sh
+++ gather-stats.sh
@@ -1,4 +1,4 @@
 #bin/bash
 nohup nvidia-smi --query-gpu=utilization.gpu --format=csv --loop=1 >> ../stats/gpu_compute_utilization.txt &
 nohup nvidia-smi --query-gpu=utilization.memory --format=csv --loop=1 >> ../stats/gpu_memory_utilization.txt &
-python gather-cpu-stats.py
+nohub python gather-cpu-stats.py &
