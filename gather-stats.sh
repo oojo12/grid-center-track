de39e6f7e49c32162d3f1dd9907eb8ae960a365b
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-12-06 09:28:48 -0500

    stop creation of nohup.txt

--- gather-stats.sh
+++ gather-stats.sh
@@ -1,4 +1,4 @@
 #bin/bash
 nohup nvidia-smi --query-gpu=utilization.gpu --format=csv --loop=1 >> ../stats/gpu_compute_utilization.txt &
 nohup nvidia-smi --query-gpu=utilization.memory --format=csv --loop=1 >> ../stats/gpu_memory_utilization.txt &
-nohub python gather-cpu-stats.py &
+nohup python gather-cpu-stats.py > /dev/null 2>&1 & 
