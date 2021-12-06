ba760d8e7feec96ac62c30c87b8af03eb91f1583
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-12-06 11:17:44 -0500

    add net stats

--- gather-stats.sh
+++ gather-stats.sh
@@ -2,3 +2,4 @@
 nohup nvidia-smi --query-gpu=utilization.gpu --format=csv --loop=1 >> ../stats/gpu_compute_utilization.txt &
 nohup nvidia-smi --query-gpu=utilization.memory --format=csv --loop=1 >> ../stats/gpu_memory_utilization.txt &
 nohup python gather-cpu-stats.py > /dev/null 2>&1 & 
+nohup python gather-net-stats.py > /dev/null 2>&1 & 
