554c6af30741ccd43bffdcb02ae564a39764da17
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-12-06 10:55:37 -0500

    Update gather-cpu-stats.py

--- gather-cpu-stats.py
+++ gather-cpu-stats.py
@@ -1,16 +1,17 @@
 import csv
 import time
 import psutil
+import subprocess
+
+
 p = psutil.Process()
 with p.oneshot():
     p.name()  # execute internal routine once collecting multiple info
     p.memory_percent()  # return cached value
     p.cpu_percent()  # return cached value
 
-with open('../stats/cpu_stats.csv', 'w', newline='') as csvfile:
-    fields = ['per_cpu_utilization', 'per_ram_utilization']
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(fields)
-    while True:
-        writer.writerow([p.memory_percent(), p.cpu_percent()])
-        time.sleep(1)
+subprocess.run('echo "per_cpu_utilization, per_ram_utilization" > testing.txt', shell=True)
+
+while True:
+    subprocess.run(f'echo "{p.memory_percent()}, {p.cpu_percent()}" >> testing.txt', shell=True)
+    time.sleep(1)
