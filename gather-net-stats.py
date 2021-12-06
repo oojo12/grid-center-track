b64a201367dc4dc31d1742dbcf7f045f1de744d0
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-12-06 11:17:09 -0500

    Create gather-net-stats.py

---
+++ gather-net-stats.py
@@ -0,0 +1,10 @@
+import time
+import psutil
+import subprocess
+
+subprocess.run('echo "packets_sent, packets_received" > ../stats/net_stats.csv', shell=True)
+
+while True:
+    net_stats = psutil.net_io_counters()
+    subprocess.run(f'echo "{net_stats.bytes_sent}, {net_stats.bytes_recv}" >> ../stats/net_stats.csv', shell=True)
+    time.sleep(1)
